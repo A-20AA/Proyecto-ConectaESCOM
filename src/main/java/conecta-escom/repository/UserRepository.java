c5788881d490d78bd4a57a42bfad9ae62c232867
A-20AA <apar593593@gmail.com>
2025-12-04 21:31:56 -0600

    Create src/ directory

---
+++ src/main/java/conecta-escom/repository/UserRepository.java
@@ -0,0 +1,8 @@
+package conecta_escom.repository;
+
+import conecta_escom.model.User;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface UserRepository extends JpaRepository<User, Integer> {
+    User findByUsername(String username);
+}
