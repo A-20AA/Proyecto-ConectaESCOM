e481184f82a00701260c5eaa76b0e5d8cd6d4b4c
A-20AA <apar593593@gmail.com>
2025-12-31 16:24:24 -0600

    General Update

--- src/main/java/conecta-escom/repository/UserRepository.java
+++
@@ -1,8 +0,0 @@
-package conecta_escom.repository;
-
-import conecta_escom.model.User;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface UserRepository extends JpaRepository<User, Integer> {
-    User findByUsername(String username);
-}
